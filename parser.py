afe81fb1028de2ead89025e11cf4625bc504bc9d
nastyuxxxa <130449872+nastyuxxxa@users.noreply.github.com>
2024-12-05 00:21:49 +0600

    Update parser.py

--- parser.py
+++ parser.py
@@ -9,8 +9,6 @@ def get_page(base_url="https://www.maxidom.ru/catalog/kran-buksy/", pages=1):  #
         for item in process_page(current_url):
             yield item
 
-        #display_products(page_num, page_data)
-
 
 def process_page(current_url):
 
@@ -29,14 +27,6 @@ def process_page(current_url):
             "title": title,
             "price": price
         }
-
-
-def display_products(page_num, products):
-    print(f"\nСобраны данные со страницы {page_num}")
-    print(f"{'№':<5}{'Название':<80}{'Цена':>10}")
-    print("-" * 95)
-    for i, product in enumerate(products, 1):
-        print(f"{i:<5}{product['title']:<90}{product['price']:>10}")
 
 
 url = "https://www.maxidom.ru/catalog/kran-buksy/"
